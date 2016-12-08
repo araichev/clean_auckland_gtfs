2a7f29afc2017b7cc98391761ad0e34b21cdd2eb
Alex Raichev <alex@raichev.net>
2016-12-08 16:17:43 +1300

    Added some packaging files

---
+++ README.rst
@@ -0,0 +1,18 @@
+Clean Auckland GTFS
+********************
+Some Python 3.4+ tools for cleaning Auckland Transport GTFS feeds
+
+
+Authors
+========
+- Alex Raichev, 2016-12-08
+
+
+Notes
+======
+- Development status is Alpha
+- This project uses semantic versioning
+
+
+History
+========
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='clean_auckland_gtfs',
+    version='0.0.0',
+    description='Python 3.4+ tools for cleaning Auckland Transport GTFS feeds',
+    long_description=readme,
+    author='Alex Raichev',
+    author_email='alex@raichev.net',
+    url='https://github.com/araichev/clean_auckland_gtfs',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+    install_requires=[
+        'gtfstk>=6.1.0',
+    ],
+)
