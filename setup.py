131042a3307b785b315e456926422d51e67636c9
Alex Raichev <araichev@users.noreply.github.com>
2017-10-17 16:23:18 +1300

    Dropped zombie stops, trips, etc in clean()

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open('LICENSE') as f:
 
 setup(
     name='clean_auckland_gtfs',
-    version='0.1.0',
+    version='0.2.0',
     description='Python 3.5+ tools for cleaning Auckland Transport GTFS feeds',
     long_description=readme,
     author='Alex Raichev',
     author_email='alex@raichev.net',
     url='https://github.com/araichev/clean_auckland_gtfs',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
         'gtfstk>=6.1.0',
     ],
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,21 +7,21 @@ from clean_auckland_gtfs import *
 feed = gt.read_gtfs(DATA_DIR/'raw_auckland_gtfs_20161122.zip', dist_units='km')
 
 def test_drop_school_routes():
-	n = feed.routes.shape[0]
-	feed1 = drop_school_routes(feed)
-	
-	# Should drop some routes
-	k = feed1.routes.shape[0]
-	assert k < n
+    n = feed.routes.shape[0]
+    feed1 = drop_school_routes(feed)
+
+    # Should drop some routes
+    k = feed1.routes.shape[0]
+    assert k < n
 
 def test_clean():
-	n = feed.routes.shape[0]
-	feed1 = clean(feed)
-	
-	# Should drop some routes
-	k = feed1.routes.shape[0]
-	assert k < n
+    n = feed.routes.shape[0]
+    feed1 = clean(feed)
 
-	# Route short names should be unique
-	j = feed1.routes.route_short_name.nunique()
-	assert j == k
+    # Should drop some routes
+    k = feed1.routes.shape[0]
+    assert k < n
+
+    # Route short names should be unique
+    j = feed1.routes.route_short_name.nunique()
+    assert j == k
